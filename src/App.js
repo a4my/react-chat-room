93435e4ade1a626be8a6d11237d230d08e7e8c28
Alex Fourmy <alexfourmy@gmail.com>
2021-11-11 16:39:43 +0100

    added LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import { ChatEngine } from 'react-chat-engine'
 import ChatFeed from './components/ChatFeed'
+import LoginForm from './components/LoginForm'
 
 import './App.css'
 
+const projectID = '37d6c40b-1cf1-41da-b73e-ea39df301a59'
+
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="37d6c40b-1cf1-41da-b73e-ea39df301a59"
-      userName="Alex"
-      userSecret="1234"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
       renderChatFeed={chatAppProps => <ChatFeed {...chatAppProps} />}
     />
   )
